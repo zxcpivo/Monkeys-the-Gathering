0ee55f91951f151fd2c95cc107b014aaa386fea3
zxcpivo <tvo9mamma@gmail.com>
2024-11-11 19:43:51 -0500

    Created Monkeys script
    Has a parent class and three child classes (ice monkey, dark monkey, sniper monkey)

---
+++ Assets/Scripts/Monkeys.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseMonkeyCard : MonoBehaviour
+{
+    public string CardName;
+    public int Attack;
+    public int Health;
+    public Sprite CardImage;
+
+    public abstract void ActivateEffect();
+}
+
+public class DartMonkey : BaseMonkeyCard
+{
+    private void Awake()
+    {
+        CardName = "Dart Monkey";
+        Attack = 1;
+        Heath = 2;
+    }
+
+    public override void ActivateEffect()
+    {
+        Debug.Log("dart monkey effect activated");
+    }
+}
+
+public class IceMonkey : BaseMonkeyCard
+{
+    private void Awake()
+    {
+        CardName = "Ice Monkey";
+        Attack = 1;
+        Health = 2;
+    }
+
+    public override void ActivateEffect() 
+    {
+        Debug.Log("ice monkey effect activated");
+    }
+}
+
+public class SniperMonkey : BaseMonkeyCard
+{
+    private void Awake()
+    {
+        CardName = "Sniper Monkey";
+        Attack = 3;
+        Health = 3;
+    }
+
+    public override void ActivateEffect()
+    {
+        Debug.Log("sniper monkey effect activated");
+    }
+}
+
