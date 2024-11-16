6aa66cd0c077accc526e181463f10e6f1a9ceb8e
S-vani <shayan.vanishoja86@gmail.com>
2024-11-15 19:16:03 -0500

    Turn System added

--- Assets/Scripts/TurnSystem.cs
+++ Assets/Scripts/TurnSystem.cs
@@ -8,21 +8,50 @@ public class TurnSystem : MonoBehaviour
 
     public bool isYourTurn;
     public int YourTurn;
-    public int isOpponentTurn;
+    public int OpponentTurn;
     public Text turnText;
+    public Text manaText;
 
     public int maxMana;
     public int currentMana;
 
-    // Start is called before the first frame update
     void Start()
     {
+        isYourTurn = true;
+        YourTurn = 1;
+        OpponentTurn = 0;
+
+        maxMana = 1;
+        currentMana = 1;
+    }
+
+    void Update()
+    {
+        if(isYourTurn == true)
+        {
+            turnText.text = "Your Turn";
+        }
+        else
+        {
+            turnText.text = "Opponent Turn";
+        }
+
+        manaText.text = currentMana + "/" + maxMana;
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void EndYourTurn()
     {
-        
+        isYourTurn = false;
+        OpponentTurn += 1;
+    }
+
+    public void EndOpponentTurn()
+    {
+        isYourTurn = true;
+        YourTurn += 1;
+
+        maxMana += 1;
+        currentMana = maxMana;
     }
 }
