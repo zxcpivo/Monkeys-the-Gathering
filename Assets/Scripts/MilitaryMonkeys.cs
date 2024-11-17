f8acdbbea91b0a138c6cdaa523a2e6c15f1ffa61
S-vani <shayan.vanishoja86@gmail.com>
2024-11-17 14:08:47 -0500

    debugging

--- Assets/Scripts/MilitaryMonkeys.cs
+++ Assets/Scripts/MilitaryMonkeys.cs
@@ -25,3 +25,4 @@ public abstract class BaseMilitaryCard : MonoBehaviour
     public abstract void ActivateEffect();
 }
 
+
--- Assets/Scripts/SniperMonkey.cs
+++ Assets/Scripts/SniperMonkey.cs
@@ -12,6 +12,8 @@ public class SniperMonkey : BaseMilitaryCard
     public override void ActivateEffect()
     {
         print($"{CardName} effect activated");
+        if (turnScript == null)
+            Debug.LogError("someObject is null!");
         print(turnScript.maxMana);
     }
 }
