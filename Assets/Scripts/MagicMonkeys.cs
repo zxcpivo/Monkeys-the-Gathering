871763c3d8074e56908cb3b0f971046c8d35bdaf
Cry86 <amirali.ghadimi25@ycdsbk12.ca>
2024-11-18 00:04:43 -0500

    Added Magic Monkey Base
    Added base for all magic cards

---
+++ Assets/Scripts/MagicMonkeys.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public abstract class BaseMagicCard : MonoBehaviour
+{
+    public string CardName;
+    public int Attack;
+    public int Health;
+    public int ManaCost;
+    public int DecreaseDamage;
+    public Sprite CardImage;
+
+    public BaseMagicCard(string name, int attack, int health, int cost, int decrease)
+    {
+        this.CardName = name;
+        this.Attack = attack;
+        this.Health = health;
+        this.ManaCost = cost;
+        this.DecreaseDamage = decrease;
+    }
+
+
+    public abstract void ActivateEffect();
+}
+
+
