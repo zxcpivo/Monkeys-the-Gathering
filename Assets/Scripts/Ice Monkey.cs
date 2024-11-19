6e84f136174a6b230be62734d047a572c4784bf9
zxcpivo <tvo9mamma@gmail.com>
2024-11-19 14:29:14 -0500

    Added an effect to the Ice Monkey

--- Assets/Scripts/Ice Monkey.cs
+++ Assets/Scripts/Ice Monkey.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class IceMonkey : BasePrimaryCard
 {
-    public int FreezeTime;
+    public TurnSystem turnScript;
 
     public IceMonkey(string name, int attack, int health, int cost, int freezeTime) : base(name, attack, health, cost)
     {
-        this.FreezeTime = freezeTime;
+
     }
 
     public override void ActivateEffect()
     {
-
+        print($"{CardName} effect activated");
+        if (turnScript.isYourTurn)
+            turnScript.YourCurrentMana -= ManaCost;
+        else
+            turnScript.OpponentCurrentMana -= ManaCost;
     }
 }
