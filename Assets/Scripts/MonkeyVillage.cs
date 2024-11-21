c9c3b7fc9903b6f0870ecd33e9496e123c622a45
Aaron Leung <159062489+Toona1@users.noreply.github.com>
2024-11-20 23:40:37 -0500

    version 1 of monkeyvillage

--- Assets/Scripts/MonkeyVillage.cs
+++ Assets/Scripts/MonkeyVillage.cs
@@ -7,17 +7,33 @@ public class MonkeyVillage : BaseSupportCard
     public int AttackBoost;
     public TurnSystem turnScript;
 
+    // Constructor remains the same
     public MonkeyVillage(string name, int attack, int health, int cost, int attackboost) : base(name, attack, health, cost)
     {
-        this.AttackBoost = attackboost; 
+        this.AttackBoost = attackboost;
     }
 
-     public override void ActivateEffect()  
+    public override void ActivateEffect()  
     {
         print($"{CardName} effect activated");
         if (turnScript.isYourTurn)
             turnScript.YourCurrentMana -= ManaCost;
         else
             turnScript.OpponentCurrentMana -= ManaCost;
+            
+        BoostMonkeyAttack();
+    }
+
+    private void BoostMonkeyAttack()
+    {
+        foreach (var card in turnScript.CardDeck)
+        {
+            BaseMonkey baseMonkey = card.GetComponent<BaseMonkey>();
+            if (baseMonkey != null) 
+            {
+                baseMonkey.Attack += AttackBoost;
+                print($"{baseMonkey.CardName}'s attack increased by {AttackBoost}!");
+            }
+        }
     }
 }
